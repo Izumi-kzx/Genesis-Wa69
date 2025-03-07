f86b5a6e44e8f0bb4996fd7291b990d8d9865882
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-06 22:31:01 -0400

    Actualizar main-menuJuegos.js

--- plugins/main-menuJuegos.js
+++ plugins/main-menuJuegos.js
@@ -8,7 +8,7 @@ import fetch from 'node-fetch'
 const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 const defaultMenu = {
-  before: `“ʜᴏʟᴀ *%name*, a qui esta todos los comandos para ganar ki⭐ "
+  before: `“ʜᴏʟᴀ *%name*\n ``` ```"
 
 
 ▬▭▬▭▬▭▬✦✧✦▬▭▬▭▬▭▬
