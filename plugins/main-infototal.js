5b7fb4ca7bd90fd9a97653e40788dfdf693ad346
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-06 22:09:27 -0400

    Actualizar main-infototal.js

--- plugins/main-infototal.js
+++ plugins/main-infototal.js
@@ -1,12 +1,68 @@
-let handler = async (m, { conn }) => {
-let totalf = Object.values(global.plugins).filter(
-    (v) => v.help && v.tags
-  ).length;
-conn.reply(m.chat, `*✅ Total de Funciones del bot* : ${totalf}`,m)
-}
+import { prepareWAMessageMedia, generateWAMessageFromContent } from '@whiskeysockets/baileys';
 
-handler.help = ['totalfunciones']
-handler.tags = ['main']
-handler.command = ['totalfunciones']
-handler.register = true
-export default handler 
+const handler = async (m, { conn }) => {
+
+let txt = `> *YouTube Play 🍧.*\n\n`;
+txt += `hola\n\n`;
+txt += `• *Duración:*\n`;
+txt += `• *Autor:*\n`;
+txt += `• *Publicado:*\n`;
+txt += `• *Url:*\n\n`;
+await conn.sendMessage(m.chat, {
+    image: { url: 'https://i.ibb.co/Rpsxjb5t/file.jpg' },
+    caption: txt,
+    footer: 'la cosa es seria chavito\nte amo w',
+    contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true
+    },
+    buttons: [
+        {
+            type: 4,
+            nativeFlowInfo: {
+                name: 'single_select',
+                paramsJson: JSON.stringify({
+                    title: '⊹₊ ⋆ᯓᡣ𐭩 rᥱsᥙᥣ𝗍ᥲძ᥆s ᥡ᥆ᥙ𝗍ᥙᑲᥱ',
+                    sections: [
+                        {
+                            title: 'my focking bicht',
+                            highlight_label: '',
+                            rows: [
+                                {
+                                    header: '⌬ Message',
+                                    title: 'menu all',
+                                    description: 'i like pussydog',
+                                    id: ".menu"
+                                },
+                                {
+                                    header: '⌬ Message',
+                                    title: 'check ping',
+                                    description: 'i like pussycat',
+                                    id: ".ping"
+                                },
+                            ],
+                        },
+                    ],
+                }),
+            },
+        },
+        {
+            buttonId: '.ping',
+            buttonText: { displayText: 'ᯓᡣ𐭩 ᥲᥙძі᥆' },
+            type: 1,
+        },
+        {
+            buttonId: '.tqto',
+            buttonText: { displayText: 'ᯓᡣ𐭩 ᥎іძᥱ᥆' },
+            type: 1,
+        },
+    ],
+    headerType: 1,
+    viewOnce: true
+}, { quoted: m });
+
+};
+
+handler.command = ['tesyt']; 
+export default handler;
