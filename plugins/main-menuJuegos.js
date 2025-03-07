5e19bb85c53554d68172262e5ddb131d75914b20
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-06 21:51:02 -0400

    Actualizar main-menuJuegos.js

--- plugins/main-menuJuegos.js
+++ plugins/main-menuJuegos.js
@@ -176,7 +176,7 @@ let img = 'https://tinyurl.com/2xragovw'
  let fkon = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
 await m.react('🐉') 
 
- conn.sendMessage(m.chat, {
+/* conn.sendMessage(m.chat, {
         text: text,
         contextInfo: {
         externalAdReply: {
@@ -187,7 +187,46 @@ await m.react('🐉')
         mediaType: 1,
         renderLargerThumbnail: true
         }}},
-        { quoted: estilo})
+        { quoted: estilo}) */
+
+await conn.sendMessage(
+      m.chat,
+      {
+        footer: text,
+        headerType: 4,
+        viewOnce: true,
+        caption: estilo(text),
+        document: fs.readFileSync("./package.json"),
+        fileName: "Sofia documento vip",
+        mimetype:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        fileLength: 99999999,
+        buttons: [
+          {
+            buttonId: `.ping`,
+            buttonText: { displayText: "ᯓᡣ𐭩 ⍴іᥒg" },
+          },
+          {
+            buttonId: `.owner`,
+            buttonText: { displayText: "ᯓᡣ𐭩 ᥆ᥕᥒᥱr" },
+          },
+        ],
+        contextInfo: {
+          externalAdReply: {
+            title: "Sofia-Ai Menu Completo",
+            body: "ᯓᡣ𐭩 Ai Sofia",
+            mediaType: 1,
+            mediaUrl: "",
+            thumbnailUrl: img,
+            sourceUrl: "",
+            containsAutoReply: true,
+            renderLargerThumbnail: true,
+            showAdAttribution: false,
+          },
+        },
+      },
+      { quoted: m }
+    );
 
   } catch (e) {
     conn.reply(m.chat, 'Lo siento, el menú en desarrollo [TEAM ⭐ ANG] .', m)
